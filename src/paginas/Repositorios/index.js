86fa8ecf154736be7b489e316ee903516a46102b
Thalisson <thalissonpop91@gmail.com>
2024-02-02 15:24:42 -0300

    feat: user repository page

---
+++ src/paginas/Repositorios/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList, TouchableOpacity } from 'react-native';
+import estilos from './estilos';
+import { pegarRepositoriosUsuario } from '../../servicos/requisicoes/repositorios';
+
+export default function Repositorios({ route, navigation }) {
+    const [repo, setRepo] = useState([]);
+
+    useEffect( async () => {
+        const resultado = await pegarRepositoriosUsuario(route.params.id);
+        setRepo(resultado);
+    }, []);
+
+    return (
+        <View style={estilos.container}>
+                <Text style={estilos.repositoriosTexto}>{repo.length} repositórios criados</Text>
+                <TouchableOpacity 
+                    style={estilos.botao}
+                    onPress={() => navigation.navigate('CriarRepositorio', {id: route.params.id})}
+                >
+                    <Text style={estilos.textoBotao}>Adicionar novo repositório</Text>
+                </TouchableOpacity>
+
+                <FlatList 
+                    data={repo}
+                    style={{ width: '100%' }}
+                    keyExtractor={repo => repo.id}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity 
+                        style={estilos.repositorio}
+                        onPress={() => navigation.navigate('InfoRepositorio', {item})}
+                        >
+                            <Text style={estilos.repositorioNome}>{item.name}</Text>
+                            <Text style={estilos.repositorioData}>Atualizar em {item.data}</Text>
+                        </TouchableOpacity>
+                    )}
+                />
+        </View>
+    );
+}
